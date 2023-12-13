722c1e9523414580e43db11725757f951b5259d2
DannyBosonit <danielandres.ortiz@bosonit.com>
2023-12-13 12:35:22 -0500

    Aprendiendo módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import {
   BrowserModule,
   provideClientHydration,
 } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { CounterModule } from './counter/components/counter.module';
+
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heores/hero/hero.component';
 import { ListComponent } from './heores/list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, HeroComponent, ListComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeroComponent, ListComponent],
+  imports: [BrowserModule, AppRoutingModule, CounterModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
