8eb6f9b951bdf88d93699cda1235c68cf0563e34
DannyBosonit <danielandres.ortiz@bosonit.com>
2023-12-15 13:12:26 -0500

    Añadir personaje flujo completo

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -18,11 +18,10 @@ export class AddCharacterComponent {
   emitCharacter() {
     console.log(this.character);
 
-    if (this.character.name === '') return;
+    if (this.character.name.length === 0) return;
 
     this.onNewCharacter.emit(this.character);
 
-    this.character.name = '';
-    this.character.power = 0;
+    this.character = { name: '', power: 0 };
   }
 }
